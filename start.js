fa94e060a165dce6ebc92609103ffe3f73321ed1
Gabriel Henrique Silva <ghsilha@gmail.com>
2020-10-02 18:00:50 -0300

    functions and groups

--- start.js
+++ start.js
@@ -8,50 +8,81 @@ const readline = require('readline').createInterface({
  * @type {object}
  */
 var respostas = getRespostas()
+var groups = []
 function whatsappBotStart () {
   wa.create().then(client => start(client))
-
-  function throwDice (sides) {
-    return Math.floor(Math.random() * Math.floor(sides)) + 1
-  }
 
   /**
    * @param {wa.Client} client
    */
   function start (client) {
     client.onMessage(message => {
-      if (respostas[message.body.toLowerCase()] !== undefined) {
-        client.sendText(message.from, respostas[message.body])
+      if (checkThisMessage(message) && isGrupoCadastrado(message)) {
+        sendAnswer(message, client)
       }
-      if (message.body.includes('/roll')) {
-        var value = 'resultados: '
-        var numbers = message.body.slice(5).split('d')
-        var numberDices = numbers[0]
-        if (numberDices >= 100) {
-          value += 'vai tomar no cu'
-          console.log(message)
-          console.log(value)
-          client.sendText(message.chatId, value)
-        } else {
-          var numberSides = numbers[1]
-          for (let dice = 0; dice < numberDices; dice++) {
-            value += throwDice(numberSides) + ', '
-          }
-          console.log(message)
-          console.log(value)
-          client.sendText(message.chatId, value)
-        }
+      if (message.body.includes('/roll') && isGrupoCadastrado(message)) {
+        calculeDices(message, client)
+      }
+      if (message.body === '/start bot') {
+        cadastrarGrupo(message, client)
       }
     })
     readline.on('line', (input) => {
       if (input === 'update') {
-        respostas = getRespostas()
-        console.log(respostas)
+        atualizarRespostas()
       }
       readline.prompt()
     })
   }
+
+  function throwDice (sides) {
+    return Math.floor(Math.random() * Math.floor(sides)) + 1
+  }
+
+  function atualizarRespostas () {
+    respostas = getRespostas()
+    console.log(respostas)
+  }
+
+  function cadastrarGrupo (message, client) {
+    groups.push(message.chatId)
+    console.log('Bot iniciado' + '->' + message.from)
+    client.sendText(message.from, 'Bot iniciado')
+  }
+
+  function sendAnswer (message, client) {
+    console.log(respostas[message.body] + '->' + message.from)
+    client.sendText(message.from, respostas[message.body])
+  }
+
+  function calculeDices (message, client) {
+    var value = 'resultados: '
+    var numbers = message.body.slice(5).split('d')
+    var numberDices = numbers[0]
+    if (numberDices >= 100) {
+      value += 'vai tomar no cu'
+      console.log(message)
+      console.log(value)
+      client.sendText(message.chatId, value)
+    } else {
+      var numberSides = numbers[1]
+      for (let dice = 0; dice < numberDices; dice++) {
+        value += throwDice(numberSides) + ', '
+      }
+      console.log(message)
+      console.log(value)
+      client.sendText(message.chatId, value)
+    }
+  }
 }
+function checkThisMessage (message) {
+  return respostas[message.body.toLowerCase()] !== undefined
+}
+
+function isGrupoCadastrado (message) {
+  return groups.includes(message.chatId)
+}
+
 /**
  * @returns {object}
  */
